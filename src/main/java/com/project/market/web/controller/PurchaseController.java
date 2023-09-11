8830a42f48663bd004063a9b5296355e1825e5b2
Mauricio Aliendre Perez <Maurcio.Aliendre@jala.university>
2023-09-11 19:21:22 -0400

    Add the origin URL to accept the CORS in the application

--- src/main/java/com/project/market/web/controller/PurchaseController.java
+++ src/main/java/com/project/market/web/controller/PurchaseController.java
@@ -9,12 +9,19 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/purchases")
+@CrossOrigin(origins = "https://apimarket-production.up.railway.app/")
 public class PurchaseController {
     @Autowired
     private PurchaseService purchaseService;
