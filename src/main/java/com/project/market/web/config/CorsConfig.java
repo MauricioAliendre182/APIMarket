e4db9544b61c8c2585a435ec7b1cdc084c4dc3e2
Mauricio Aliendre Perez <Maurcio.Aliendre@jala.university>
2023-09-11 19:51:55 -0400

    Add the origin URL to accept the CORS in the application with originPatterns

---
+++ src/main/java/com/project/market/web/config/CorsConfig.java
@@ -0,0 +1,24 @@
+//package com.project.market.web.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class CorsConfig {
+//
+//    // We will include a Bean
+//    @Bean
+//    public WebMvcConfigurer corsConfigurer() {
+//        return new WebMvcConfigurer() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//                registry.addMapping("/**") // Define the URL patterns for which CORS should be allowed
+//                        .allowedOrigins("https://apimarket-production.up.railway.app") // Replace with your frontend domain
+//                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+//                        .allowCredentials(true); // If you need to allow cookies
+//            }
+//        };
+//    }
+//}
