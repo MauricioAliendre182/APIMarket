680cd93d07070cb623ca88b3af3043acd08ff182
Mauricio Aliendre Perez <Maurcio.Aliendre@jala.university>
2023-10-13 23:10:14 -0400

    Add the CORS configuration

--- src/main/java/com/project/market/web/config/CorsConfig.java
+++ src/main/java/com/project/market/web/config/CorsConfig.java
@@ -3,25 +3,40 @@ package com.project.market.web.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
 
 @Configuration
 public class CorsConfig {
 
     // We will include a Bean
+//    @Bean
+//    public WebMvcConfigurer corsConfigurer() {
+//        return new WebMvcConfigurer() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//                registry.addMapping("/**") // Define the URL patterns for which CORS should be allowed
+//                        .allowedOrigins("http://localhost:4200") // Replace with your frontend domain
+//                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+//                        .allowCredentials(true) // If you need to allow cookies
+//                        .allowedHeaders(HttpHeaders.CONTENT_TYPE,
+//                                        HttpHeaders.AUTHORIZATION);
+//            }
+//        };
+//    }
+
     @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**") // Define the URL patterns for which CORS should be allowed
-                        .allowedOrigins("http://localhost:4200") // Replace with your frontend domain
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
-                        .allowCredentials(true) // If you need to allow cookies
-                        .allowedHeaders(HttpHeaders.CONTENT_TYPE,
-                                        HttpHeaders.AUTHORIZATION);
-            }
-        };
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST", "PUT", "DELETE"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
