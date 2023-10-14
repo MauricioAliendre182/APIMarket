d23aa5caff8401769c9396d64671d0f2415041a8
Mauricio Aliendre Perez <Maurcio.Aliendre@jala.university>
2023-10-13 23:44:03 -0400

    Add the CORS configuration with taking into account the security

--- src/main/java/com/project/market/security/WebSecurityConfig.java
+++ src/main/java/com/project/market/security/WebSecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.channel.ChannelProcessingFilter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
@@ -51,6 +52,7 @@ public class WebSecurityConfig{
                         .authenticated())
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(new CorsFilter(), ChannelProcessingFilter.class)
                 .addFilterBefore(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class)
                 .addFilter(jwtAuthenticationFilter)
                 .build();
